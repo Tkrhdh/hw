ab54a5c9f0bf4aaafb2cd66532296f1fb2766bb6
”Darya <darya-tkachenko-99@mail.ru>
2023-05-25 17:55:17 +0300

    fix

--- homework1/src/main/java/Application.java
+++ homework1/src/main/java/Application.java
@@ -4,20 +4,19 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Application {
     public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+
         Kotik barsik = new Kotik(5, "Barsik",3);
         barsik.liveAnotherDay();
         System.out.println("barsik name : "+ barsik.getName() );
         System.out.println("barsik weight : "+ barsik.getWeight());
         barsik.setMeow("Мяу Мяу");
-
         Kotik kitty = new Kotik();
         kitty.setKotik(10, "Ruby",2, "Мяу" );
+        System.out.println("Создан котику с именем " + kitty.getName());
         kitty.setMeow("Мяу");
         System.out.println("kitty name : "+ kitty.getName());
         System.out.println("kitty weight : "+ kitty.getWeight());
         System.out.println("Равенство meow котов "+barsik.getMeow().equals(kitty.getMeow()));
-
-
         System.out.println("Количество созданных котиков: "+ Kotik.getkotikCount());
 
 
